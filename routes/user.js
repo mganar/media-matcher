595e4b6eaf51b894febb3ceb564dd0d41ed26d1c
Ricky-Truckner <70228416+Ricky-Truckner@users.noreply.github.com>
2023-11-28 14:02:12 -0800

    added user route

---
+++ routes/user.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+
+router.post('/', async (req, res) => {
+    const user = new User({
+      id: req.params.id,
+      title: req.body.title,
+      genre: req.body.genre,
+      description: req.body.description,
+      isLiked: req.body.isLiked
+    });
+  
+    try {
+      const newUser = await user.save();
+      res.json(newUser);
+    } catch (err) {
+      res.json({ message: "Cannot create new User" });
+    }
+  })
+
+  async function getMediaInfo(req, res, next)
+{
+    let user
+    try{
+
+        media = await User.findById(req.params.id)
+        if (user == null) {
+            return res.json({ message: 'Cannot find User' })
+        }
+    } catch (err) {
+        res.json({ message: err.message })
+    }
+    res.user = user
+    next()
+}
+
+module.exports = router
